f0940598a2952e86035f054d869397a8fa077385
Albert Shefner <usn0emo@gmail.com>
2025-06-07 10:50:33 +0300

    Use explicit variable in html short macros

--- examples/html/basic.c
+++ examples/html/basic.c
@@ -11,20 +11,20 @@ int main() {
     StringBuilder sb = {0};
     sb.alloc = new_malloc_allocator();
 
-    Html html = html_begin();
+    Html h = html_begin();
 
-    h_head({
-        html_title_cstr(&html, "My page");
+    h_head(h, {
+        html_title_cstr(&h, "My page");
     });
 
-    h_body({
-        h_h1({
-             html_text_cstr(&html, "Hello, World!");
+    h_body(h, {
+        h_h1(h, {
+             html_text_cstr(&h, "Hello, World!");
         });
     });
 
-    html_end(&html);
-    html_render_to_sb_and_free(&html, &sb);
+    html_end(&h);
+    html_render_to_sb_and_free(&h, &sb);
 
     printf(SV_FMT, SV_ARG(sb));
 }
